8b1d64c67bf6906940479f3af5b6e9520990f386
bjc1102 <bjc30849@gmail.com>
2023-01-28 16:48:49 +0900

    fix: 사용안하는 extension build 제거

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,8 +6,6 @@ const srcDir = path.join(__dirname, "..", "src");
 module.exports = {
   entry: {
     popup: path.join(srcDir, "popup.tsx"),
-    background: path.join(srcDir, "background.ts"),
-    content_script: path.join(srcDir, "content_script.tsx"),
   },
   output: {
     path: path.join(__dirname, "../dist/js"),
